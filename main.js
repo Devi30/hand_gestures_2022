89351eda92214dce9bbdd58a53988c15da617675
Devi30 <96772778+Devi30@users.noreply.github.com>
2022-12-10 15:42:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,3 +23,40 @@ function speak(){
     var utterthis=new SpeechSynthesisUtterance(speakdata1+speakdata2)
     synth.speak(utterthis)
 }
+function check(){
+    img=document.getElementById("captured_image")
+    classifier.classify(img,gotresult)
+}
+var prediction1=""
+var prediction2=""
+function gotresult(error,results){
+   if(error){
+    console.error(error)
+   } 
+   else{
+    console.log(results)
+    prediction1=results[0].label
+    prediction2=results[1].label
+    document.getElementById("result_gesture_name1").innerHTML=prediction1
+    document.getElementById("result_gesture_name2").innerHTML=prediction2
+    speak()
+    if(prediction1=="Heart"){
+        document.getElementById("update_emoji1").innerHTML="&#9829"
+    } 
+    if(prediction1=="Victory"){
+        document.getElementById("update_emoji1").innerHTML="&#9996"
+    }  
+    if(prediction1=="Thumbs Up"){
+        document.getElementById("update_emoji1").innerHTML="&#128077"
+    }   
+    if(prediction2=="Heart"){
+        document.getElementById("update_emoji2").innerHTML="&#9829"
+    } 
+    if(prediction2=="Victory"){
+        document.getElementById("update_emoji2").innerHTML="&#9996"
+    }  
+    if(prediction2=="Thumbs Up"){
+        document.getElementById("update_emoji2").innerHTML="&#128077"
+    }   
+   }
+}
